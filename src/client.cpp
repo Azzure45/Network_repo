60e5ed629a702661c3ad2d025c4dc30148c89b90
Zizii2 <meglado20@gmail.com>
2025-05-13 11:01:22 +0000

    fixed varible name from client_fd to client_addr

--- src/client.cpp
+++ src/client.cpp
@@ -5,6 +5,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
 using std::cerr;
 using std::cout;
 
@@ -14,28 +17,34 @@ int main(void){
         cerr << "Failed to create socket\n";
         return 1;
     }
-    int optval = 1;
-    if(setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0 ){
-        cerr << "Failed to change options\n";
-        return 1;
-    }
+    // int optval = 1;
+    // if(setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0 ){
+    //     cerr << "Failed to change options\n";
+    //     close(client_fd);
+    //     return 1;
+    // }
     struct sockaddr_in client_addr;
-    memset(&client_fd, 0, sizeof(client_fd));
+    memset(&client_addr, 0, sizeof(client_addr));
     client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons(8080);
+    client_addr.sin_port = htons(PORT);
+    // client_addr.sin_addr = "172.16.217.188";
     inet_pton(AF_INET, "172.16.217.188", &client_addr.sin_addr);
 
-    int status = bind(client_fd, (struct sockaddr *)&client_addr, sizeof(client_addr));
-    //do some error check
-
-    if(listen(client_fd, 5) < 0){
-        cerr << "failed to listen for server\n";
+    std::cout << "Connecting to server..." << std::endl;
+    if (connect(client_fd, (struct sockaddr*)&client_addr, sizeof(client_addr)) < 0) {
+        std::cerr << "Connection failed" << std::endl;
+        close(client_fd);
         return 1;
     }
-    struct sockaddr_in server_addr;
-    socklen_t server_addr_len = sizeof(server_addr);
 
-    int server_fd = accept(client_fd, (struct sockaddr *)&server_addr, &server_addr_len);
+    // int server_fd = accept(client_fd, (struct sockaddr *)&server_addr, &server_addr_len);
+    char buffer[BUFFER_SIZE] = "AnSa\n";
+    // int valread = read(sock, buffer, BUFFER_SIZE);
+    send(client_fd, buffer, sizeof(char) * sizeof(buffer), 0);
+    while(true){
+        cout << "yipiy\n";
+        ;;
+    }
 
     close(client_fd);
 }
